456bb86a8ade0b740d7fb64f60307074939c1efb
Eduardo Nunes <eduardonunes2525@gmail.com>
2022-12-23 13:11:13 -0300

    feat: Adds react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1>Error 404</h1>,
+    // children: [],
+  },
+]);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
   document.getElementById('root'),
 );
