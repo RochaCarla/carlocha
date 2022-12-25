275c1015cb4f7d1f1f523c45d6376527562f7308
Eduardo Nunes <eduardonunes2525@gmail.com>
2022-12-25 12:12:29 -0300

    feat: Adds react-markdown plugin to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import Markdown from '@pity/vite-plugin-react-markdown';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 3000 },
-  plugins: [react()],
+  plugins: [react(), Markdown()],
 });
