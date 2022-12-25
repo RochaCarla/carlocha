6c802d13fc3e3e82e8fc5b4fedd849a9808f8eaa
Eduardo Nunes <eduardonunes2525@gmail.com>
2022-12-25 12:17:09 -0300

    feat: Adds global style

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,19 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    name: string;
+    color: {
+      background: string;
+      background_profile_card: string;
+      primary: string;
+      pure_primary: string;
+      secondary: string;
+      shadow: string;
+
+      title: string;
+      text: string;
+      text_inverse: string;
+    };
+  }
+}
