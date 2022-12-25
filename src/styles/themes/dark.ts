396cad656b04138b2baa6935ecbf38204df6642e
Eduardo Nunes <eduardonunes2525@gmail.com>
2022-12-25 12:18:50 -0300

    feat: Adds dark and light themes

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,15 @@
+export default {
+  name: 'dark',
+  color: {
+    primary: 'linear-gradient(90deg, #66CAB6 0%, #66CAB6 100%)',
+    pure_primary: '#66CAB6',
+    secondary: '#3FB7D1',
+    background: '#222128',
+    background_profile_card: '#282B37',
+    shadow: 'rgb(0 0 0 / 0.2)',
+
+    title: '#FFFCFC',
+    text: '#FFFCFC',
+    text_inverse: '#282B37',
+  },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,16 @@
+export default {
+  name: 'light',
+  color: {
+    // primary: '#339989',
+    primary: 'linear-gradient(90deg, rgba(237,180,101,1) 0%, rgba(228,149,109,1) 100%)',
+    pure_primary: 'rgba(237, 180, 101, 1)',
+    background: '#E9EAF0',
+    background_profile_card: '#FFFAFB',
+    secondary: '#a833b9',
+    shadow: 'rgb(0 0 0 / 0.2)',
+
+    title: '#2d3047',
+    text: '#2d3047',
+    text_inverse: '#a833b9',
+  },
+};
